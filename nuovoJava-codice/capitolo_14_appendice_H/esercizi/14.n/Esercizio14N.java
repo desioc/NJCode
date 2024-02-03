55f0c41494a38d99e63b626bb9b8a091404afe22
desioc <61605795+desioc@users.noreply.github.com>
2024-02-03 18:44:25 +0100

    Update Esercizio14N.java

--- nuovoJava-codice/capitolo_14_appendice_H/esercizi/14.n/Esercizio14N.java
+++ nuovoJava-codice/capitolo_14_appendice_H/esercizi/14.n/Esercizio14N.java
@@ -6,7 +6,7 @@ public class Esercizio14N {
         list.ensureCapacity(1000000);
         long startTime = System.currentTimeMillis();
         for (int i = 1; i <= 2000000; ++i) {
-            if (i%2 == 0) {
+            if (i % 2 == 0) {
                 list.add(i);
             }
         }
